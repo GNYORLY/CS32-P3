e951ecf182f0aa8699053159bf10412a53f86370
Jeannie Chiem <gnyorly@gmail.com>
2016-02-17 02:05:45 -0800

    dirt and rock psuedocode

--- Actor.h
+++ Actor.h
@@ -5,7 +5,7 @@
 
 class ppl
 {
-	//things that can be annoyed(people)/ things with health
+	//things that can be annoyed
 	//reg protester, hardcore protester, frackman
 	void getAnnoyed() 
 	{ 
@@ -42,7 +42,7 @@ public:
 
 };
 
-class FrackMan : public Actor
+class FrackMan : public Actor  //one of frackmans base classes must have a pointer to studentworld via getWorld()(a f() from the base class that returns the pointer)
 {
 public:
 	FrackMan() : Actor(0, 30, 60, right, 10) //5 water, 1 sonar, 0 gold
@@ -60,6 +60,22 @@ public:
 			Make a digging sound by playing the SOUND_DIG sound (see the StudentWorld section of this document for details on how to play a sound).
 		else
 			check if a key was pressed:
+				char ch;
+				if (getWorld()->getKey(ch) == true)
+				{
+				// user hit a key this tick!
+				switch (ch)
+				{
+				case KEY_PRESS_LEFT:
+				... move player to the left ...;
+				break;
+				case KEY_PRESS_RIGHT:
+				... move player to the left ...;
+				break;
+				case KEY_PRESS_SPACE:
+				... add a Squirt in front of the player...;
+				break;
+				// etc…
 			if (escape was pressed)
 				set frackman to completely annoyed(dead)--studentworld should respond accordingly to the dead frackman(replay lvl or end game when no lives left)
 			if (spacebar was pressed)
@@ -107,10 +123,27 @@ public:
 
 };
 
-class rock
+class rock//can be set dead but not annoyed
 {
-public:
-
+public: //rock image, (x,y) location specified by studentworld, start out in a stable state, face down, depth 1, size 1.0, setVisible(true)
+	/*doSomething()
+	{
+		if (dead)
+			return;
+		if (stable)
+			check if theres any dirt in the four spaces directly below it (e.g. if rock is at (20,30), check  (20,29), (21,29), (22,29) and (23,29))
+			if (there is ANY dirt there)
+				do nothing
+			else ///no dirt there
+				rock must transition into a waiting state for the next 30 ticks
+				if (rock is in a waiting state and 30 ticks passed)
+					transition rock to falling state
+					play falling rock sound
+					if (rock is in a falling state)
+						move down one square each tick until it falls to the edge, on another rock, or on dirt
+						set dead
+	}
+	*/
 };
 
 class gold 
@@ -131,12 +164,11 @@ public:
 
 };
 
-class dirt
+class dirt //cannot be annoyed , no mention of dead
 {
-public:
-
+public: // image, (x,y) location passed by student world during construction of the whole field, facing right, depth 3, size .25(1x1), setVisible(true) ///size 1.0 is (4x4)
+	//just sits in place really / doSomething() would do nothing
+	//cannot be annoyed (not part of ppl class)
 };
-
-// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
 #endif // ACTOR_H_
