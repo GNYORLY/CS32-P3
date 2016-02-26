8ef2f4762ccc5254e4c4aa273a15383e1311c527
Jeannie Chiem <gnyorly@gmail.com>
2016-02-25 16:28:38 -0800

    sonar, water, dropgold

--- Actor.cpp
+++ Actor.cpp
@@ -1,6 +1,12 @@
 #include "Actor.h"
 #include "StudentWorld.h"
-
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
 
 void FrackMan::doSomething()
 {
@@ -13,16 +19,43 @@ void FrackMan::doSomething()
 		switch (button)
 		{
 		case KEY_PRESS_LEFT:
-			moveTo(getX() - 1, getY());
+			if (getDirection() == left && getX() > 0)
+				moveTo(getX() - 1, getY());
+			else
+				setDirection(left);
 			break;
 		case KEY_PRESS_RIGHT:
-			moveTo(getX() + 1, getY());
+			if (getDirection() == right && getX() < 60)
+				moveTo(getX() + 1, getY());
+			else
+				setDirection(right);
 			break;
 		case KEY_PRESS_UP:
-			moveTo(getX(), getY() + 1);
+			if (getDirection() == up && getY() < 60)
+				moveTo(getX(), getY() + 1);
+			else
+				setDirection(up);
 			break;
 		case KEY_PRESS_DOWN:
-			moveTo(getX(), getY() - 1);
+			if (getDirection() == down && getY() > 0)
+				moveTo(getX(), getY() - 1);
+			else
+				setDirection(down);
+			break;
+		case 'Z':
+		case 'z':
+			if (m_sonar > 0)
+			{
+				getWorld()->useSonar();
+				m_sonar--;
+			}
+			break;
+		case KEY_PRESS_TAB:
+			if (m_gold > 0)
+			{
+				getWorld()->placeGold();
+				m_gold--;
+			}
 			break;
 		}
 	}
@@ -46,13 +79,65 @@ void FrackMan::doSomething()
 	if (same direction && frackman is able to move in that direction(no rocks in the way and not out of the game area))
 	use graphObject's moveTo() to move 1 square in that direction
 	////////////valid positions are from 0-60 inclusive for both x and y, frackman cannot go <= radius of 3 from center of rock////////
-	if (z or Z && frackman has at least 1 sonar)
-	use sonar to illuminate within a radius of 12 around him (includes 11.99)
-	decrease sonar count by 1
-	setVisible() all hidden game objects within a radius of 12
-	if (tab && frackman has at least 1 gold)
-	add gold object to the current x,y location
-	reduce gold count by 1
+	
 	////////the gold nugget will start out visible but only live for 100 ticks, it can only be picked up by protesters/////////
 	*/
 }
+
+void activeStuff::doSomething()
+{
+	if (isDead())
+		return;
+
+	if (m_visible == false && getWorld()->frckRadius(getX(), getY(), 4)) //cant use isVisible()
+	{
+		m_visible = true;
+		setVisible(true);
+		return;
+	}
+	else if (getWorld()->frckRadius(getX(), getY(), 3))
+	{
+		setDead();
+		getWorld()->playSound(m_sound);
+		getWorld()->increaseScore(m_points);
+		changeAmount();
+	}
+}
+
+void oil::changeAmount()
+{
+	getWorld()->removeOil();
+}
+
+void gold::changeAmount()
+{
+	getWorld()->giveGold();
+}
+
+tempGoodie::tempGoodie(StudentWorld* sw, int image, int x, int y, int points) : activeStuff(sw, image, x, y, points)
+{
+	m_lifeTime = max(100, signed(300 - 10 * getWorld()->getLevel()));
+}
+
+void tempGoodie::doSomething()
+{
+	if (m_lifeTime <= 0)
+	{
+		setDead();
+	}
+	else
+		m_lifeTime--;
+	
+	activeStuff::doSomething();
+}
+
+void sonar::changeAmount()
+{
+	getWorld()->giveSonar();
+}
+
+void water::changeAmount()
+{
+	getWorld()->giveWater();
+}
+
